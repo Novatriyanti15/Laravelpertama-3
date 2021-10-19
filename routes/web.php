995fab0745c0213623d76214d1a7bf5b0c7aa441
Novatriyanti15 <kanjengmasrajaassidiq@gmail.com>
2021-10-19 21:08:43 +0700

    commit3

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('', [App\Http\Controllers\cobacontroller::class, 'index']);
+Route::get('/friends', [App\Http\Controllers\cobacontroller::class, 'index']);
+Route::get('/friends/create', [App\Http\Controllers\cobacontroller::class, 'create']);
+Route::post('/friends', [App\Http\Controllers\cobacontroller::class, 'store']);
